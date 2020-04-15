5695fe3c1d43c1d3189ee4adf7accd5e47044dff
Kamaldwip Choudhury (V) <kamaldwip_choudhury@apple.com>
2020-04-15 02:13:54 -0500

    Http GET for Customer Service with Spring Rest Template

---
+++ src/main/java/com/kamaldwip/springframework/kamalbreweryclient/web/client/CustomerClient.java
@@ -0,0 +1,36 @@
+package com.kamaldwip.springframework.kamalbreweryclient.web.client;
+
+import com.kamaldwip.springframework.kamalbreweryclient.web.model.CustomerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "kamal.client",ignoreInvalidFields = false)
+public class CustomerClient {
+
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
+
+    private RestTemplate restTemplate;
+
+
+    public CustomerClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    private String apiHost;
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+
+    public CustomerDto getCustomerById(UUID customerId){
+        return restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + customerId.toString(), CustomerDto.class);
+    }
+
+
+
+}
---
+++ src/test/java/com/kamaldwip/springframework/kamalbreweryclient/web/client/CustomerClientTest.java
@@ -0,0 +1,24 @@
+package com.kamaldwip.springframework.kamalbreweryclient.web.client;
+
+import com.kamaldwip.springframework.kamalbreweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomerClientTest {
+
+    @Autowired
+    CustomerClient customerClient;
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = customerClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+
+    }
+}
